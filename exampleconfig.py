46cf0a8919570f85f4a83a48dfbe44fd7d45306e
brutewoorse17 <147273128+brutewoorse17@users.noreply.github.com>
2023-10-09 00:19:10 +0800

    update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -12,7 +12,7 @@ class Development(Config):
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
     STRING_SESSION = "Your value"
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "Your value"
+    TG_BOT_TOKEN = "6429499802:AAEAHRPVdNn-a0bm75lFgTsa4dxD4Qlp1k4"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
     PRIVATE_GROUP_BOT_API_ID = -100
     # command handler
